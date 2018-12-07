b8969826a2a4472f32f675853ec6d5fbb6278592
mirceanegoitaivfuture <mircea.negoita@ivfuture.ro>
2018-12-07 20:39:55 +0200

    Added main layout + router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import { Provider } from "react-redux";
+import store from './store';
+
+import Navbar from './components/Navbar';
+import About from './components/About';
+import Contact from './components/Contact';
+import Post from './components/Post';
+import Posts from './components/Posts';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <BrowserRouter>
+          <Provider store={store}>
+            <div>
+              <Navbar/>
+              <Switch>
+                <Route path="/" component={Posts}></Route>
+                <Route path="/post" component={Post}></Route>
+                <Route path="/about" component={About}></Route>
+                <Route path="/contact" component={Contact}></Route>
+              </Switch>
+            </div>
+          </Provider>
+        </BrowserRouter>
+
+
       </div>
     );
   }
