2e711ee04f128c26fc24d09a2b7f80500320dc7c
mirceanegoitaivfuture <mircea.negoita@ivfuture.ro>
2018-12-07 20:37:32 +0200

    Renamed class to className

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,27 +5,27 @@ class Navbar extends Component{
     render(){
         return(
             <div>
-                <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                    <a class="navbar-brand" href="index.html">Mircea's Blog</a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                    <a className="navbar-brand" href="index.html">Mircea's Blog</a>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                     </button>
                 
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav ml-auto">
-                        <li class="nav-item">
-                            <a class="nav-link" href="">
-                                <div class="btn btn-primary">Posts</div>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav ml-auto">
+                        <li className="nav-item">
+                            <a className="nav-link" href="">
+                                <div className="btn btn-primary">Posts</div>
                             </a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="">
-                                <div class="btn btn-primary">About</div>
+                        <li className="nav-item">
+                            <a className="nav-link" href="">
+                                <div className="btn btn-primary">About</div>
                             </a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="">
-                                <div class="btn btn-primary">Contact</div>
+                        <li className="nav-item">
+                            <a className="nav-link" href="">
+                                <div className="btn btn-primary">Contact</div>
                             </a>
                         </li>
                     </ul>
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -5,49 +5,49 @@ class Posts extends Component {
         return(
             <div>
                 
-                <div class="container" id="intro">
+                <div className="container" id="intro">
                     <h1>Mircea's Blog</h1>
                     <h2>Awesome Ideas</h2>
                 </div>
 
-                <div class="container content">
-                    <div class="row">
-                        <div class="col-md-8">
-                            <div class="jumbotron">
-                                <h1 class="display-4">A blog post</h1>    
-                                <hr class="my-4"/>
-                                <p class="lead">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat deleniti adipisci mollitia dolores ratione, eum esse eveniet cumque vitae, explicabo omnis, quo debitis maiores? Iste ratione tempore exercitationem accusamus harum.</p>
-                                <a class="btn btn-primary btn-lg" href="post.html" role="button">Read more</a>
+                <div className="container content">
+                    <div className="row">
+                        <div className="col-md-8">
+                            <div className="jumbotron">
+                                <h1 className="display-4">A blog post</h1>    
+                                <hr className="my-4"/>
+                                <p className="lead">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat deleniti adipisci mollitia dolores ratione, eum esse eveniet cumque vitae, explicabo omnis, quo debitis maiores? Iste ratione tempore exercitationem accusamus harum.</p>
+                                <a className="btn btn-primary btn-lg" href="post.html" role="button">Read more</a>
                             </div>
-                            <div class="jumbotron">
-                                <h1 class="display-4">Another blog post</h1>
-                                <hr class="my-4"/>
-                                <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero est corporis nostrum autem odio sed nesciunt suscipit aperiam debitis quidem aliquam recusandae nobis quaerat, eius placeat cupiditate obcaecati non ullam?</p>
-                                <a class="btn btn-primary btn-lg" href="post.html" role="button">Read more</a>
+                            <div className="jumbotron">
+                                <h1 className="display-4">Another blog post</h1>
+                                <hr className="my-4"/>
+                                <p className="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero est corporis nostrum autem odio sed nesciunt suscipit aperiam debitis quidem aliquam recusandae nobis quaerat, eius placeat cupiditate obcaecati non ullam?</p>
+                                <a className="btn btn-primary btn-lg" href="post.html" role="button">Read more</a>
                             </div>
-                            <div class="jumbotron">
-                                <h1 class="display-4">Yet another one!</h1>
-                                <hr class="my-4"/>
-                                <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem iure ipsa possimus blanditiis provident, dolorum quisquam quos iste quam vitae aliquam nostrum, laboriosam sed modi aliquid adipisci corporis expedita debitis.</p>
-                                <a class="btn btn-primary btn-lg" href="post.html" role="button">Read more</a>
+                            <div className="jumbotron">
+                                <h1 className="display-4">Yet another one!</h1>
+                                <hr className="my-4"/>
+                                <p className="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem iure ipsa possimus blanditiis provident, dolorum quisquam quos iste quam vitae aliquam nostrum, laboriosam sed modi aliquid adipisci corporis expedita debitis.</p>
+                                <a className="btn btn-primary btn-lg" href="post.html" role="button">Read more</a>
                             </div>
                         </div>
-                        <div class="col-md-4">
-                            <div class="card" style="width: 18rem;">
-                                <img class="card-img-top" src="http://via.placeholder.com/268x180" alt="Card image cap"/>
-                                <div class="card-body">
-                                    <h5 class="card-title">Card title</h5>
-                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                    <a href="post.html" class="btn btn-primary">Go somewhere</a>
+                        <div className="col-md-4">
+                            <div className="card" style="width: 18rem;">
+                                <img className="card-img-top" src="http://via.placeholder.com/268x180" alt="Card image cap"/>
+                                <div className="card-body">
+                                    <h5 className="card-title">Card title</h5>
+                                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    <a href="post.html" className="btn btn-primary">Go somewhere</a>
                                 </div>
                             </div>
                             <br/>
-                            <div class="card" style="width: 18rem;">
-                                <img class="card-img-top" src="http://via.placeholder.com/268x180" alt="Card image cap"/>
-                                <div class="card-body">
-                                    <h5 class="card-title">Card title</h5>
-                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                            <div className="card" style="width: 18rem;">
+                                <img className="card-img-top" src="http://via.placeholder.com/268x180" alt="Card image cap"/>
+                                <div className="card-body">
+                                    <h5 className="card-title">Card title</h5>
+                                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    <a href="#" className="btn btn-primary">Go somewhere</a>
                                 </div>
                             </div>
                         </div>
