beb3f01971ed8e54c031cf9e559d9594c8af6198
mircea <mircea.negoita13@gmail.com>
2018-12-08 01:03:58 +0200

    Fixed react warnings

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -36,7 +36,7 @@ class Contact extends Component{
                         <div className="col-md-4">
                             <div className="col-md-4">
                                 <div className="card" style={{width: 18 + 'rem'}}>
-                                    <img className="card-img-top" src="http://via.placeholder.com/268x180" alt="Card image cap"></img>
+                                    <img className="card-img-top" src="http://via.placeholder.com/268x180" alt="Card cap"></img>
                                     <div className="card-body">
                                         <h5 className="card-title">Card title</h5>
                                         <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -45,11 +45,11 @@ class Contact extends Component{
                                 </div>
                                 <br></br>
                                 <div className="card" style={{width: 18 + 'rem'}}>
-                                    <img className="card-img-top" src="http://via.placeholder.com/268x180" alt="Card image cap"></img>
+                                    <img className="card-img-top" src="http://via.placeholder.com/268x180" alt="Card cap"></img>
                                     <div className="card-body">
                                         <h5 className="card-title">Card title</h5>
                                         <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                        <a href="#" className="btn btn-primary">Go somewhere</a>
+                                        <a href="post.html" className="btn btn-primary">Go somewhere</a>
                                     </div>
                                 </div>
                             </div>
