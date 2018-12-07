c66ac27ffa13fc28f51a010c4692b6eabf2dd93d
mirceanegoitaivfuture <mircea.negoita@ivfuture.ro>
2018-12-07 20:34:33 +0200

    Added Post static component

---
+++ src/components/Post.js
@@ -0,0 +1,29 @@
+import React,{Component} from 'react';
+
+class Post extends Component{
+    render(){
+        return(
+            <div>
+                <div class="container" id="intro">
+                    <h1>A blog post</h1>
+                    <h2>Subtitle</h2>
+                </div>
+
+                <div class="container content">
+                    <div class="row">
+                        <div class="jumbotron">
+                            <p class="lead">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat deleniti adipisci mollitia dolores ratione, eum esse eveniet cumque vitae, explicabo omnis, quo debitis maiores? Iste ratione tempore exercitationem accusamus harum.</p>
+                            <p class="lead">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat deleniti adipisci mollitia dolores ratione, eum esse eveniet cumque vitae, explicabo omnis, quo debitis maiores? Iste ratione tempore exercitationem accusamus harum.</p>
+                            <p class="lead">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione ea voluptatem, sequi atque, quaerat voluptatum neque ipsam facere ut, officia expedita voluptate. Modi voluptatum eaque quasi odio, hic expedita ullam eos reiciendis, itaque nulla deleniti facilis, iusto illum autem veritatis. Dolorem delectus nihil, quaerat praesentium officia illum laboriosam? Voluptatibus dolores ipsam nam ea enim porro accusamus iste, impedit, autem exercitationem voluptatum sint magni sed molestias. Nemo mollitia adipisci illo eos? Aut maiores iure qui nesciunt dolore voluptatem totam quasi et molestias similique. Ipsa dolor quae possimus natus, veritatis, rem enim assumenda quis unde totam voluptatum? Deserunt sit assumenda dicta provident?</p>
+                            <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus obcaecati corrupti unde laboriosam! Praesentium veniam sit doloribus tempora, dignissimos natus, ducimus consequatur ut recusandae saepe consequuntur perspiciatis, aperiam atque fuga? Quaerat consequuntur ullam in repudiandae, sint quod nulla assumenda optio eveniet ut quo facere corporis molestias ad dolores amet sapiente!</p>
+                            <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum debitis consequatur iure, vero placeat ducimus explicabo voluptas aut facilis perferendis soluta dicta velit vel incidunt illum, possimus, ullam ut magni animi sapiente in. Autem nostrum odit incidunt eligendi non. Ad, eaque laborum, reprehenderit dicta rerum iusto, magnam nesciunt architecto corporis vitae officiis at quisquam accusamus. Amet labore, aliquid atque veniam corrupti, omnis numquam id, error animi repellendus distinctio soluta tempora facilis placeat libero! Porro quis cum vitae eum nam molestiae in sit omnis possimus illum accusantium, est adipisci commodi nesciunt iste aliquam veniam nulla natus fuga recusandae animi libero. Iste?</p>
+                            <p class="lead">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat voluptatum consectetur optio ipsum omnis a, laboriosam porro in illum asperiores provident dolorum deleniti maiores, libero voluptatem quasi ut dolor. Nihil molestias laudantium harum eligendi adipisci, similique sed ab molestiae, asperiores doloribus dolorem perferendis excepturi quas sint sequi sapiente doloremque accusamus!</p>                        
+                        </div>
+                    </div>  
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Post;
